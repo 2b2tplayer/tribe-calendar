468e59c4fe5079fd1bb5339e037f2d798b7e9c19
tokyo <42143007+2b2tplayer@users.noreply.github.com>
2025-04-12 22:05:54 -0300

    allow for development

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ const app = express();
 app.use(express.json());
 
 // Habilitar CORS
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000'
+  ]
+}));
 
 // Montar rutas
 app.use('/api/auth', authRoutes);
